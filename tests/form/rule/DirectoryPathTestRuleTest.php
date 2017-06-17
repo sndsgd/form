8de9a1291b2606facb5257ce033c487f0272fea1
Russell <sndsgd@users.noreply.github.com>
2017-06-17 16:35:35 -0400

    added FilesystemPathTestRule (#9)

---
+++ tests/form/rule/DirectoryPathTestRuleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace sndsgd\form\rule;
+
+class DirectoryPathTestRuleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+	new DirectoryPathTestRule();
+    }
+}
