c76f36f7fb5cd033e0db09311ecff3633da95147
Russell <sndsgd@users.noreply.github.com>
2017-08-25 08:05:43 -0400

    add Form::merge() (#11)

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -6,7 +6,7 @@ class FormTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstructor()
     {
-        new \sndsgd\Form();
+        new Form();
     }
 
     /**
@@ -14,6 +14,68 @@ class FormTest extends \PHPUnit_Framework_TestCase
      */
     public function testConstructorException()
     {
-        new \sndsgd\Form("names-are-not-allowed");
+        new Form("names-are-not-allowed");
+    }
+
+    /**
+     * @dataProvider provideMerge
+     */
+    public function testMerge($form, array $forms, $expectFields)
+    {
+        $form->merge(...$forms);
+        $allFields = $form->getFields();
+
+        foreach ($expectFields as $field) {
+            $this->assertArrayHasKey($field, $allFields);
+        }
+    }
+
+    public function provideMerge(): array
+    {
+        return [
+            [
+                $this->createForm("one"),
+                [
+                    $this->createForm("two"),
+                    $this->createForm("three"),
+                ],
+                ["one", "two", "three"],
+            ],
+            [
+                $this->createForm("one", "two"),
+                [
+                    $this->createForm("three"),
+                    $this->createForm("four"),
+                ],
+                ["one", "two", "three", "four"],
+            ],
+            [
+                $this->createForm(),
+                [
+                    $this->createForm("one"),
+                    $this->createForm("two"),
+                    $this->createForm("three"),
+                ],
+                ["one", "two", "three"],
+            ],
+            [
+                $this->createForm(),
+                [
+                    $this->createForm("one", "two"),
+                    $this->createForm("three"),
+                    $this->createForm("four", "five"),
+                ],
+                ["one", "two", "three", "four", "five"],
+            ],
+        ];
+    }
+
+    private function createForm(string ...$fields): Form
+    {
+        $ret = new Form();
+        foreach ($fields as $name) {
+            $ret->addFields(new \sndsgd\form\field\StringField($name));
+        }
+        return $ret;
     }
 }
