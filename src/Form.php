289c443af5467e6e2a430a0b1e1676262ab68a65
Russell <r@snds.gd>
2017-08-25 07:55:24 -0400

    add Form::merge()

--- src/Form.php
+++ src/Form.php
@@ -19,4 +19,20 @@ class Form extends \sndsgd\form\field\ObjectField
         }
         $this->name = "";
     }
+
+    /**
+     * Merge the fields of one or more forms into this one
+     *
+     * @param \sndsgd\Form $forms The forms to merge
+     * @return \sndsgd\Form
+     */
+    public function merge(\sndsgd\Form ...$forms): Form
+    {
+        foreach ($forms as $form) {
+            $fields = array_values($form->getFields());
+            $this->addFields(...$fields);
+        }
+
+        return $this;
+    }
 }
