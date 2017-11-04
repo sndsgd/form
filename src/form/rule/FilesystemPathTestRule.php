f9aa3e32ff95d8f521d97a3e98991e577537bc52
Russell <r@snds.gd>
2017-11-04 10:18:32 -0400

    fix indentation

--- src/form/rule/FilesystemPathTestRule.php
+++ src/form/rule/FilesystemPathTestRule.php
@@ -24,8 +24,8 @@ class FilesystemPathTestRule extends RuleAbstract
 
     public function __construct(bool $isDir = false, int $tests = \sndsgd\Fs::EXISTS)
     {
-	$this->isDir = $isDir;
-	$this->tests = $tests | \sndsgd\Fs::EXISTS;
+        $this->isDir = $isDir;
+        $this->tests = $tests | \sndsgd\Fs::EXISTS;
     }
 
     /**
@@ -33,17 +33,17 @@ class FilesystemPathTestRule extends RuleAbstract
      */
     public function getDescription(): string
     {
-	$chars = "";
-	$testTypes = $this->getTestTypes();
-	foreach (["r", "w", "x"] as $char) {
-	    $chars .= isset($testTypes[$char]) ? $char : "-";
-	}
+        $chars = "";
+        $testTypes = $this->getTestTypes();
+        foreach (["r", "w", "x"] as $char) {
+            $chars .= isset($testTypes[$char]) ? $char : "-";
+        }
 
-	if ($this->isDir) {
-	    return sprintf(_("directory path:%s"), $chars);
-	}
+        if ($this->isDir) {
+            return sprintf(_("directory path:%s"), $chars);
+        }
 
-	return sprintf(_("file path:%s"), $chars);
+        return sprintf(_("file path:%s"), $chars);
     }
 
     /**
@@ -51,29 +51,29 @@ class FilesystemPathTestRule extends RuleAbstract
      */
     public function getErrorMessage(): string
     {
-	if ($this->errorMessage) {
-	    return $this->errorMessage;
-	}
+        if ($this->errorMessage) {
+            return $this->errorMessage;
+        }
 
-	$testTypes = array_values($this->getTestTypes());
-	switch (count($testTypes)) {
-	    case 0:
-		return $this->isDir ? _("must be a directory") : _("must be a file");
-	    case 1:
-		$desc = $testTypes[0];
-		break;
-	    case 2:
-		$desc = $testTypes[0]." "._("and")." ".$testTypes[1];
-		break;
-	    default:
-		$desc = \sndsgd\Arr::implode(", ", $testTypes, _("and")." ");
-	}
+        $testTypes = array_values($this->getTestTypes());
+        switch (count($testTypes)) {
+            case 0:
+                return $this->isDir ? _("must be a directory") : _("must be a file");
+            case 1:
+                $desc = $testTypes[0];
+                break;
+            case 2:
+                $desc = $testTypes[0]." "._("and")." ".$testTypes[1];
+                break;
+            default:
+                $desc = \sndsgd\Arr::implode(", ", $testTypes, _("and")." ");
+        }
 
-	if ($this->isDir) {
-	    return sprintf(_("must be a %s directory"), $desc);
-	}
+        if ($this->isDir) {
+            return sprintf(_("must be a %s directory"), $desc);
+        }
 
-	return sprintf(_("must be a %s file"), $desc);
+        return sprintf(_("must be a %s file"), $desc);
     }
 
     /**
@@ -83,40 +83,40 @@ class FilesystemPathTestRule extends RuleAbstract
      */
     public function getTestTypes(): array
     {
-	$ret = [];
-	if ($this->tests & \sndsgd\Fs::READABLE) {
-	    $ret["r"] = _("readable");
-	}
-	if ($this->tests & \sndsgd\Fs::WRITABLE) {
-	    $ret["w"] = _("writable");
-	}
-	if (!$this->isDir && $this->tests & \sndsgd\Fs::EXECUTABLE) {
-	    $ret["x"] = _("executable");
-	}
-	return $ret;
+        $ret = [];
+        if ($this->tests & \sndsgd\Fs::READABLE) {
+            $ret["r"] = _("readable");
+        }
+        if ($this->tests & \sndsgd\Fs::WRITABLE) {
+            $ret["w"] = _("writable");
+        }
+        if (!$this->isDir && $this->tests & \sndsgd\Fs::EXECUTABLE) {
+            $ret["x"] = _("executable");
+        }
+        return $ret;
     }
 
     /**
      * @inheritDoc
      */
     public function validate(
-	&$value,
-	\sndsgd\form\Validator $validator = null
+        &$value,
+        \sndsgd\form\Validator $validator = null
     ): bool
     {
-	if ($value instanceof \sndsgd\fs\entity\EntityInterface) {
-	    $entity = $value;
-	} elseif ($this->isDir) {
-	    $entity = \sndsgd\fs::dir($value);
-	} else {
-	    $entity = \sndsgd\fs::file($value);
-	}
+        if ($value instanceof \sndsgd\fs\entity\EntityInterface) {
+            $entity = $value;
+        } elseif ($this->isDir) {
+            $entity = \sndsgd\fs::dir($value);
+        } else {
+            $entity = \sndsgd\fs::file($value);
+        }
 
-	if (!$entity->test($this->tests)) {
-	    return false;
-	}
+        if (!$entity->test($this->tests)) {
+            return false;
+        }
 
-	$value = $entity;
-	return true;
+        $value = $entity;
+        return true;
     }
 }
--- tests/form/rule/FilesystemPathTestRuleTest.php
+++ tests/form/rule/FilesystemPathTestRuleTest.php
@@ -9,41 +9,41 @@ class FilesystemPathTestRuleTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetDescription($isDir, $tests, $expect)
     {
-	$rule = new FilesystemPathTestRule($isDir, $tests);
-	$this->assertSame($expect, $rule->getDescription());
+        $rule = new FilesystemPathTestRule($isDir, $tests);
+        $this->assertSame($expect, $rule->getDescription());
     }
 
     public function providerGetDescription()
     {
-	$e = \sndsgd\Fs::EXISTS;
-	$r = \sndsgd\Fs::READABLE;
-	$w = \sndsgd\Fs::WRITABLE;
-	$x = \sndsgd\Fs::EXECUTABLE;
+        $e = \sndsgd\Fs::EXISTS;
+        $r = \sndsgd\Fs::READABLE;
+        $w = \sndsgd\Fs::WRITABLE;
+        $x = \sndsgd\Fs::EXECUTABLE;
 
-	return [
-	    [false, 0, "file path:---"],
-	    [false, $e, "file path:---"],
-	    [false, $r, "file path:r--"],
-	    [false, $e | $r, "file path:r--"],
-	    [false, $w, "file path:-w-"],
-	    [false, $e | $w, "file path:-w-"],
-	    [false, $x, "file path:--x"],
-	    [false, $e | $x, "file path:--x"],
-	    [false, $r | $w | $x, "file path:rwx"],
-	    [false, $e | $r | $w | $x, "file path:rwx"],
-	    [true, 0, "directory path:---"],
-	    [true, $e, "directory path:---"],
-	    [true, $r, "directory path:r--"],
-	    [true, $w, "directory path:-w-"],
-	];
+        return [
+            [false, 0, "file path:---"],
+            [false, $e, "file path:---"],
+            [false, $r, "file path:r--"],
+            [false, $e | $r, "file path:r--"],
+            [false, $w, "file path:-w-"],
+            [false, $e | $w, "file path:-w-"],
+            [false, $x, "file path:--x"],
+            [false, $e | $x, "file path:--x"],
+            [false, $r | $w | $x, "file path:rwx"],
+            [false, $e | $r | $w | $x, "file path:rwx"],
+            [true, 0, "directory path:---"],
+            [true, $e, "directory path:---"],
+            [true, $r, "directory path:r--"],
+            [true, $w, "directory path:-w-"],
+        ];
     }
 
     public function testGetErrorMessageCustom()
     {
-	$rule = new FilesystemPathTestRule();
-	$expect = "custom error message";
-	$rule->setErrorMessage($expect);
-	$this->assertSame($expect, $rule->getErrorMessage());
+        $rule = new FilesystemPathTestRule();
+        $expect = "custom error message";
+        $rule->setErrorMessage($expect);
+        $this->assertSame($expect, $rule->getErrorMessage());
     }
 
     /**
@@ -51,33 +51,33 @@ class FilesystemPathTestRuleTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetErrorMessage($isDir, $tests, $expect)
     {
-	$rule = new FilesystemPathTestRule($isDir, $tests);
-	$this->assertSame($expect, $rule->getErrorMessage());
+        $rule = new FilesystemPathTestRule($isDir, $tests);
+        $this->assertSame($expect, $rule->getErrorMessage());
     }
 
     public function providerGetErrorMessage()
     {
-	$e = \sndsgd\Fs::EXISTS;
-	$r = \sndsgd\Fs::READABLE;
-	$w = \sndsgd\Fs::WRITABLE;
-	$x = \sndsgd\Fs::EXECUTABLE;
+        $e = \sndsgd\Fs::EXISTS;
+        $r = \sndsgd\Fs::READABLE;
+        $w = \sndsgd\Fs::WRITABLE;
+        $x = \sndsgd\Fs::EXECUTABLE;
 
-	return [
-	    [false, 0, "must be a file"],
-	    [false, $e, "must be a file"],
-	    [false, $r, "must be a readable file"],
-	    [false, $e | $r, "must be a readable file"],
-	    [false, $w, "must be a writable file"],
-	    [false, $e | $w, "must be a writable file"],
-	    [false, $x, "must be a executable file"], // ugh
-	    [false, $e | $x, "must be a executable file"], // ugh
-	    [false, $r | $w, "must be a readable and writable file"],
-	    [false, $r | $w | $x, "must be a readable, writable, and executable file"],
-	    [false, $e | $r | $w | $x, "must be a readable, writable, and executable file"],
+        return [
+            [false, 0, "must be a file"],
+            [false, $e, "must be a file"],
+            [false, $r, "must be a readable file"],
+            [false, $e | $r, "must be a readable file"],
+            [false, $w, "must be a writable file"],
+            [false, $e | $w, "must be a writable file"],
+            [false, $x, "must be a executable file"], // ugh
+            [false, $e | $x, "must be a executable file"], // ugh
+            [false, $r | $w, "must be a readable and writable file"],
+            [false, $r | $w | $x, "must be a readable, writable, and executable file"],
+            [false, $e | $r | $w | $x, "must be a readable, writable, and executable file"],
 
-	    [true, $r, "must be a readable directory"],
+            [true, $r, "must be a readable directory"],
 
-	];
+        ];
     }
 
     /**
@@ -85,36 +85,36 @@ class FilesystemPathTestRuleTest extends \PHPUnit_Framework_TestCase
      */
     public function testValidate($isDir, $tests, $path, $expect)
     {
-	$rule = new FilesystemPathTestRule($isDir, $tests);
-	$this->assertSame($expect, $rule->validate($path));
+        $rule = new FilesystemPathTestRule($isDir, $tests);
+        $this->assertSame($expect, $rule->validate($path));
     }
 
     public function providerValidate()
     {
-	$e = \sndsgd\Fs::EXISTS;
-	$r = \sndsgd\Fs::READABLE;
-	$w = \sndsgd\Fs::WRITABLE;
+        $e = \sndsgd\Fs::EXISTS;
+        $r = \sndsgd\Fs::READABLE;
+        $w = \sndsgd\Fs::WRITABLE;
 
-	return [
-	    # file
-	    [false, $r, __FILE__, true],
-	    [false, $e | $r, __FILE__, true],
-	    [false, $e | $r | $w, __FILE__, true],
-	    [false, $r, "/does/not/exist", false],
-	    [false, $e | $r, "/does/not/exist", false],
-	    [false, $e | $r | $w, "/does/not/exist", false],
-	    [false, $r, \sndsgd\Fs::file(__FILE__), true],
-	    [false, $r, \sndsgd\Fs::file("/does/not/exist"), false],
+        return [
+            # file
+            [false, $r, __FILE__, true],
+            [false, $e | $r, __FILE__, true],
+            [false, $e | $r | $w, __FILE__, true],
+            [false, $r, "/does/not/exist", false],
+            [false, $e | $r, "/does/not/exist", false],
+            [false, $e | $r | $w, "/does/not/exist", false],
+            [false, $r, \sndsgd\Fs::file(__FILE__), true],
+            [false, $r, \sndsgd\Fs::file("/does/not/exist"), false],
 
-	    # directory
-	    [true, $r, __DIR__, true],
-	    [true, $e | $r, __DIR__, true],
-	    [true, $e | $r | $w, __DIR__, true],
-	    [true, $r, "/does/not/exist", false],
-	    [true, $e | $r, "/does/not/exist", false],
-	    [true, $e | $r | $w, "/does/not/exist", false],
-	    [true, $r, \sndsgd\Fs::dir(__DIR__), true],
-	    [true, $r, \sndsgd\Fs::file("/does/not/exist"), false],
-	];
+            # directory
+            [true, $r, __DIR__, true],
+            [true, $e | $r, __DIR__, true],
+            [true, $e | $r | $w, __DIR__, true],
+            [true, $r, "/does/not/exist", false],
+            [true, $e | $r, "/does/not/exist", false],
+            [true, $e | $r | $w, "/does/not/exist", false],
+            [true, $r, \sndsgd\Fs::dir(__DIR__), true],
+            [true, $r, \sndsgd\Fs::file("/does/not/exist"), false],
+        ];
     }
 }
